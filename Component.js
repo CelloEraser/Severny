3e1686ca2bcaac100b404de36ff43136d2e2856c
CelloEraser <92920304+CelloEraser@users.noreply.github.com>
2022-01-23 18:48:02 -0500

    Update Component.js

--- Component.js
+++ Component.js
@@ -1,1 +1,12 @@
-
+function insertAfter(newNode, existingNode) {
+    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+}
+export function $(item) {
+    const node = document.createElement(item);
+    return node;
+}
+export function _(item, where)
+{
+    insertAfter(item, where);
+    return item;
+}
