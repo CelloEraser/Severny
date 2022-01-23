1f47ad1107b6016440e5e3af56fa8310bd2951a2
CelloEraser <92920304+CelloEraser@users.noreply.github.com>
2022-01-23 18:55:28 -0500

    Update Operators.js

--- Functional/Operators.js
+++ Functional/Operators.js
@@ -1,1 +1,9 @@
-
+export function f(string, value = 0, func = () => {})
+{
+    const stringarray = string.split(" ");
+    const operator = stringarray[1];
+    if(operator == "|>")
+    {
+        return func(value);
+    }
+}
