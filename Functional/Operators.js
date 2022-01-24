bc5a2d4d121ba38be8f82a7d9981f4b1e52b41d6
CelloEraser <92920304+CelloEraser@users.noreply.github.com>
2022-01-24 05:47:22 -0500

    Update Operators.js

--- Functional/Operators.js
+++ Functional/Operators.js
@@ -1,9 +1,10 @@
-export function f(string, value = 0, func = () => {})
-{
-    const stringarray = string.split(" ");
-    const operator = stringarray[1];
-    if(operator == "|>")
-    {
-        return func(value);
+export function f(strings, x = 0, func = () => {}, func2 = () => {}) {
+    const operator = strings[1].replace(/\s/gi, "");
+  
+    if (operator == "|>") {
+      return func(x);
+    }
+    if (operator == ">>" {
+        return func(func2);
     }
 }
